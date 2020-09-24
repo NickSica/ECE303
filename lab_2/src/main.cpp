f18ea7e41a6cdd60ddcc63e4904c50e759c86e45
NickSica <Xoepe@hotmail.com>
2020-09-24 12:10:47 -0400

    Experiments 1 and 2

---
+++ lab_2/src/main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+
+int led = 13;
+int intensity = 0;
+
+void setup() 
+{
+    pinMode(led, OUTPUT);
+    analogWrite(led, LOW);
+    Serial.begin(9600);
+}
+
+void loop() 
+{
+    if(Serial.available() > 0)
+    {
+        Serial.print("Please enter a number from 0 to 255: ");
+        intensity = Serial.parseInt();
+        analogWrite(led, intensity);
+    }
+}
