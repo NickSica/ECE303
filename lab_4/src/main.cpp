aa91791e83a855de868bbbfe5558f3182b306632
NickSica <njs82@drexel.edu>
2020-10-07 20:36:32 -0400

    lab 4

--- lab_4/src/main.cpp
+++ lab_4/src/main.cpp
@@ -4,8 +4,10 @@ int led = 6;
 
 void setup() 
 {
+    pinMode(led, OUTPUT);
+
     noInterrupts();
-    // Setup fast PWM timer 1 channel A pin 11
+    // Setup fast PWM timer 4 channel A pin 6
     TCCR4A = 0;
     TCCR4B = 0;
     TIMSK4 = 0;
@@ -15,9 +17,8 @@ void setup()
     TCCR4A |= (1 << WGM41);
     TCCR4A |= (1 << COM4A1);
     TCCR4B |= (1 << WGM43);
-    TCCR4B |= (0 << CS41) | (0 << CS40); // 1024 prescalar
-    TIMSK4 |= (1 << OCIE4A);
-    pinMode(led, OUTPUT);
+    TCCR4B |= (0 << CS41) | (1 << CS40);
+    //TIMSK4 |= (1 << OCIE4A);
     
     Serial.begin(9600);
     interrupts();
@@ -25,25 +26,22 @@ void setup()
 
 void loop() 
 {
-    if(Serial.available() > 0)
+    if(OCR4A <= 12500)
     {
-        if(OCR4A <= 12500)
-        {
-            delay(2000);
-            Serial.println("Duty Cycle, LED Resistor Voltage, Photocell Resistor Voltage");
+        delay(2000);
+        Serial.println("Duty Cycle, LED Resistor Voltage, Photocell Resistor Voltage");
 
-            int duty_cycle = (int)((float)OCR4A / (float)ICR4 * 100);
-            Serial.println(duty_cycle);
+        int duty_cycle = (int)((float)OCR4A / (float)ICR4 * 100);
+        Serial.println(duty_cycle);
 
-            int led_resistor_value = analogRead(A1);
-            float led_resistor_voltage = (float)photo_resistor_value * (5.0 / 1023.0);
-            Serial.println(led_resistor_voltage);
+        int led_resistor_value = analogRead(A0);
+        float led_resistor_voltage = (float)led_resistor_value * (5.0 / 1023.0);
+        Serial.println(led_resistor_voltage);
 
-            int photo_resistor_value = analogRead(A0);
-            float photo_resistor_voltage = (float)photo_resistor_value * (5.0 / 1023.0);
-            Serial.println(photo_resistor_voltage);
-            Serial.println();
-            OCR4A += 625;
-        }
+        int photo_resistor_value = analogRead(A1);
+        float photo_resistor_voltage = (float)photo_resistor_value * (5.0 / 1023.0);
+        Serial.println(photo_resistor_voltage);
+        Serial.println();
+        OCR4A += 625;
     }
 }
